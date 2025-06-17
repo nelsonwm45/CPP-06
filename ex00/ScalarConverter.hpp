4257e2501ee846f19f786a75df4024cc11a0f128
nelsonwm45 <nelsonwm45@gmail.com>
2025-06-17 13:21:59 +0800

    Added ScalarConverter

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,30 @@
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <stdexcept> // for std::exception
+
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define BLUE "\033[34m"
+# define YELLOW "\033[33m"
+# define CYAN "\033[36m"
+# define MAGENTA "\033[35m"
+# define WHITE "\033[37m"
+# define RESET "\033[0m"
+
+
+
+class	ScalarConverter
+{
+	private:
+
+	
+	public:
+
+
+};
+
+std::ostream	&operator<<(std::ostream &output, const Bureaucrat &other);
+
+#endif
